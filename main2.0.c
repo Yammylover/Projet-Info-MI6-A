b16d217fb9d6c1582428c73a8f8c7e64cc3263a5
Kristal <gregoire.marie1@etu.cyu.fr>
2025-05-01 18:34:09 +0200

    Delete main2.0.c

--- main2.0.c
+++
@@ -1,9 +0,0 @@
-#include "structures.h"
-#include "code.h"
-
-int main(){
-	IDperso phan,flan;
-	strcpy(phan.nom,"Phan");
-	strcpy(flan.nom,"flan");
-	//initialisation de l'atk, la def, la vit, les pv, la dext, l'agl et l'act
-	
