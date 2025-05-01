c12ac81179f9ca7721e481bd34ff6e124288ea9f
Kristal <gregoire.marie1@etu.cyu.fr>
2025-05-01 18:34:48 +0200

    Delete af2.h

--- af2.h
+++
@@ -1,7 +0,0 @@
-#include "structures.h"
-
-
-void displayfighter(Combattant n){
-	printf("\n%s\n-HP:%d\n-ATK:%d\n-DEF:%d\n-Agilité:%d\n-Dextérité:%d\n-Action:%d\n",n.base.nom,n.pv,n.atk,n.def,n.agl,n.dex,n.act);
-
-}
