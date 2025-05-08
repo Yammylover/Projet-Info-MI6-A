5d400e52cdd59d9fcb0852d44beb874dc156999b
Kristal <gregoire.marie1@etu.cyu.fr>
2025-05-08 18:37:04 +0200

    Create code.h

---
+++ gregpatch1.2.3/code.h
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+
+
+int IDmax(int* tab,int taille){
+    int max=0;
+    int id;
+    for (int i=0; i<taille; i++){
+        if (tab[i]>max){
+            max=tab[i];
+            id=i;
+        }
+    }
+    return id;
+}
+
+int selection(int* tab,int size, char* phrase){
+	//vérification
+	if(tab==NULL){
+		printf("ERREUR FONCTION SELECTION");
+	}
+	for(int i=0;i<size;i++){
+	    printf("%d/",tab[i]);
+	}
+	//process
+	int a=0,b=0;
+	do{
+		printf("%s\n",phrase);
+		if(scanf("%d",&a)!=1){
+		    printf("Erreur saisie des données\n");
+		    exit(8);
+		}
+		for(int  i=0; i<size; i++){
+			if(a==tab[i]){
+				b=1;
+			}
+		}
+	} while(b!=1);
+	return a;
+}
