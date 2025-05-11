2a23f0dd1e84761d855fcbdb7880a6200a825cbf
Kristal <gregoire.marie1@etu.cyu.fr>
2025-05-11 13:39:42 +0200

    Update capacites.c

--- capacites.c
+++ capacites.c
@@ -8,7 +8,7 @@ void soin(Combattant* tab, int ID, int soin){
     }
     //process
     tab[ID].pv+=soin;
-    printf("%s récupère %d pv!\n",tab[ID].base.nom,soin);
+    printf("%s|%d| récupère %d pv!\n",tab[ID].base.nom,tab[ID].ID+1,soin);
     if(tab[ID].pv > tab[ID].base.pv){
         tab[ID].pv = tab[ID].base.pv;
     }
@@ -50,23 +50,23 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
         break;
         
         case 5001:  //capacités de lambda
-            printf("%s utilise pose offensive!\n",tab[n].base.nom);
+            printf("%s|%d| utilise pose offensive!\n",tab[n].base.nom,tab[n].ID+1);
             e.type=2;
             e.puissance=tab[n].base.atk;
             e.duree=6;
             addeffect(e,tab,n);
-            printf("%s gagne %d atk!\n",tab[n].base.nom,e.puissance);
+            printf("%s|%d| gagne %d atk!\n",tab[n].base.nom,tab[n].ID+1,e.puissance);
         break;
         case 5002:
-            printf("%s utilise pose deffensive!\n",tab[n].base.nom);
+            printf("%s|%d| utilise pose defensive!\n",tab[n].base.nom,tab[n].ID+1);
             e.type=3;
             e.puissance=tab[n].base.def;
             e.duree=6;
             addeffect(e,tab,n);
-            printf("%s gagne %d def!\n",tab[n].base.nom,e.puissance);
+            printf("%s|%d| gagne %d def!\n",tab[n].base.nom,tab[n].ID+1,e.puissance);
         break;
         case 5003:
-            printf("%s utilise repos!\n",tab[n].base.nom);
+            printf("%s|%d| utilise repos!\n",tab[n].base.nom,tab[n].ID+1);
             soin(tab,n,50);
         break;
         
@@ -76,7 +76,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             } else {
                 cib=cible(tab,t1,tmax,n,1,2);
             }
-            printf("%s utilise percée!\n",tab[n].base.nom);
+            printf("%s|%d| utilise percée!\n",tab[n].base.nom,tab[n].ID+1);
             r=attaque(tab,n,cib,20);
             //printf("r=%d\n",r);
             switch(r){
@@ -93,30 +93,30 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             }
         break;
         case 5005:
-            printf("%s utilise aiguisage!\n",tab[n].base.nom);
+            printf("%s|%d| utilise aiguisage!\n",tab[n].base.nom,tab[n].ID+1);
             e.type=2;
             e.puissance=tab[n].base.atk*25/100;
             e.duree=6;
             addeffect(e,tab,n);
-            printf("%s gagne %d atk!\n",tab[n].base.nom,e.puissance);
+            printf("%s|%d| gagne %d atk!\n",tab[n].base.nom,tab[n].ID+1,e.puissance);
             e.type=6;
             e.puissance=tab[n].base.dex;
             e.duree=6;
             addeffect(e,tab,n);
-            printf("%s gagne %d dex!\n",tab[n].base.nom,e.puissance);
+            printf("%s|%d| gagne %d dex!\n",tab[n].base.nom,tab[n].ID+1,e.puissance);
         break;
         case 5006:
-            printf("%s utilise étirement!\n",tab[n].base.nom);
+            printf("%s|%d| utilise étirement!\n",tab[n].base.nom,tab[n].ID+1);
             e.type=4;
             e.puissance=tab[n].base.vit*50/100;
             e.duree=3;
             addeffect(e,tab,n);
-            printf("%s gagne %d vit!\n",tab[n].base.nom,e.puissance);
+            printf("%s|%d| gagne %d vit!\n",tab[n].base.nom,tab[n].ID+1,e.puissance);
             e.type=5;
             e.puissance=tab[n].base.agl;
             e.duree=3;
             addeffect(e,tab,n);
-            printf("%s gagne %d agl!\n",tab[n].base.nom,e.puissance);
+            printf("%s|%d| gagne %d agl!\n",tab[n].base.nom,tab[n].ID+1,e.puissance);
         break;
         
         case 5007:  //capacités d'omega
@@ -125,12 +125,12 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             } else {
                 cib=cible(tab,t1,tmax,n,1,2);
             }
-            printf("%s utilise affaiblissement sur %s !\n",tab[n].base.nom,tab[cib].base.nom);
+            printf("%s|%d| utilise affaiblissement sur %s|%d| !\n",tab[n].base.nom,tab[n].ID+1,tab[cib].base.nom,tab[cib].ID+1);
             e.type=-2;
             e.puissance=-tab[cib].base.atk*50/100;
             e.duree=6;
             addeffect(e,tab,cib);
-            printf("%s perd %d atk!\n",tab[cib].base.nom,-e.puissance);
+            printf("%s|%d| perd %d atk!\n",tab[cib].base.nom,tab[cib].ID+1,-e.puissance);
         break;
         case 5008:
             if(tab[n].ID<t1){
@@ -138,15 +138,15 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             } else {
                 cib=cible(tab,t1,tmax,n,2,2);
             }
-            printf("%s utilise bouclier sur %s !\n",tab[n].base.nom,tab[cib].base.nom);
+            printf("%s|%d| utilise bouclier sur %s|%d| !\n",tab[n].base.nom,tab[n].ID+1,tab[cib].base.nom,tab[cib].ID+1);
             e.type=3;
             e.puissance=tab[cib].base.def*200/100;
             e.duree=3;
             addeffect(e,tab,cib);
-            printf("%s gagne %d def!\n",tab[cib].base.nom,e.puissance);
+            printf("%s|%d| gagne %d def!\n",tab[cib].base.nom,tab[cib].ID+1,e.puissance);
         break;
         case 5009:
-            printf("%s utilise soin de groupe!\n",tab[n].base.nom);
+            printf("%s|%d| utilise soin de groupe!\n",tab[n].base.nom,tab[n].ID+1);
             if(tab[n].ID<t1){
                 for(int i=0;i<t1;i++){
                     if(tab[i].pv>0){
@@ -169,12 +169,12 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             } else {
                 cib=cible(tab,t1,tmax,n,2,2);
             }
-            printf("%s utilise élan sur %s !\n",tab[n].base.nom,tab[cib].base.nom);
+            printf("%s|%d| utilise élan sur %s|%d| !\n",tab[n].base.nom,tab[n].ID+1,tab[cib].base.nom,tab[cib].ID+1);
             e.type=4;
             e.puissance=tab[cib].base.vit;
             e.duree=3;
             addeffect(e,tab,cib);
-            printf("%s gagne %d vit!\n",tab[cib].base.nom,e.puissance);
+            printf("%s|%d| gagne %d vit!\n",tab[cib].base.nom,tab[cib].ID+1,e.puissance);
         break;
         case 5011:
             if(tab[n].ID<t1){
@@ -182,7 +182,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             } else {
                 cib=cible(tab,t1,tmax,n,2,2);
             }
-            printf("%s utilise soin sur %s !\n",tab[n].base.nom,tab[cib].base.nom);
+            printf("%s|%d| utilise soin sur %s|%d| !\n",tab[n].base.nom,tab[n].ID+1,tab[cib].base.nom,tab[cib].ID+1);
             soin(tab,cib,40);
         break;
         case 5012:
@@ -191,12 +191,12 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             } else {
                 cib=cible(tab,t1,tmax,n,1,2);
             }
-            printf("%s utilise ralentissement sur %s !\n",tab[n].base.nom,tab[cib].base.nom);
+            printf("%s|%d| utilise ralentissement sur %s|%d| !\n",tab[n].base.nom,tab[n].ID+1,tab[cib].base.nom,tab[cib].ID+1);
             e.type=-4;
             e.puissance=-tab[cib].base.vit*50/100;
             e.duree=3;
             addeffect(e,tab,cib);
-            printf("%s perd %d vit!\n",tab[cib].base.nom,-e.puissance);
+            printf("%s|%d| perd %d vit!\n",tab[cib].base.nom,tab[cib].ID+1,-e.puissance);
         break;
         
         case 5013:  //capacités de alpha
@@ -205,7 +205,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             } else {
                 cib=cible(tab,t1,tmax,n,1,2);
             }
-            printf("%s utilise vampire!\n",tab[n].base.nom);
+            printf("%s|%d| utilise vampire!\n",tab[n].base.nom,tab[n].ID+1);
             r=attaque(tab,n,cib,100);
             //printf("r=%d\n",r);
             switch(r){
@@ -225,7 +225,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
         break;
         
         case 5014:
-            printf("%s utilise commandement!\n",tab[n].base.nom);
+            printf("%s|%d| utilise commandement!\n",tab[n].base.nom,tab[n].ID+1);
             e.type=2;
             e.duree=5;
             if(tab[n].ID<t1){
@@ -233,7 +233,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
                     if(tab[i].pv>0){
                         e.puissance=tab[i].base.atk*50/100;
                         addeffect(e,tab,i);
-                        printf("%s gagne %d atk!\n",tab[i].base.nom,e.puissance);
+                        printf("%s|%d| gagne %d atk!\n",tab[i].base.nom,tab[i].ID+1,e.puissance);
                     }
                 }
             } else {
@@ -241,28 +241,28 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
                     if(tab[i].pv>0){
                         e.puissance=tab[i].base.atk*50/100;
                         addeffect(e,tab,i);
-                        printf("%s gagne %d atk!\n",tab[i].base.nom,e.puissance);
+                        printf("%s|%d| gagne %d atk!\n",tab[i].base.nom,tab[i].ID+1,e.puissance);
                     }
                 }
             }
         break;
         case 5015:
-            printf("%s utilise inspiration!\n",tab[n].base.nom);
+            printf("%s|%d| utilise inspiration!\n",tab[n].base.nom,tab[n].ID+1);
             e.type=4;
             e.puissance=tab[n].base.vit;
             e.duree=1;
             addeffect(e,tab,n);
-            printf("%s gagne %d vit!\n",tab[n].base.nom,e.puissance);
+            printf("%s|%d| gagne %d vit!\n",tab[n].base.nom,tab[n].ID+1,e.puissance);
             e.type=5;
             e.puissance=100;
             e.duree=1;
             addeffect(e,tab,n);
-            printf("%s gagne %d agl!\n",tab[n].base.nom,e.puissance);
+            printf("%s|%d| gagne %d agl!\n",tab[n].base.nom,tab[n].ID+1,e.puissance);
             e.type=6;
             e.puissance=100;
             e.duree=1;
             addeffect(e,tab,n);
-            printf("%s gagne %d dex!\n",tab[n].base.nom,e.puissance);
+            printf("%s|%d| gagne %d dex!\n",tab[n].base.nom,tab[n].ID+1,e.puissance);
         break;
         //capacités beta
         case 5016:
@@ -271,7 +271,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             } else {
                 cib=cible(tab,t1,tmax,n,1,2);
             }
-            printf("%s utilise coup violent!\n",tab[n].base.nom);
+            printf("%s|%d| utilise coup violent!\n",tab[n].base.nom,tab[n].ID+1);
             r=attaque(tab,n,cib,100);
             switch(r){
                 case 1:
@@ -287,16 +287,16 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             }
         break;
         case 5017: 
-            printf("%s utilise barricade!\n",tab[n].base.nom);
+            printf("%s|%d| utilise barricade!\n",tab[n].base.nom,tab[n].ID+1);
             e.type=3;
             e.puissance=50;
             e.duree=2;
             addeffect(e,tab,n);
-            printf("%s gagne %d def!",tab[n].base.nom,e.puissance);
+            printf("%s|%d| gagne %d def!",tab[n].base.nom,tab[n].ID+1,e.puissance);
             soin(tab,n,30);
         break;
         case 5018:
-            printf("%s utilise trépignement!\n",tab[n].base.nom);
+            printf("%s|%d| utilise trépignement!\n",tab[n].base.nom,tab[n].ID+1);
             if(tab[n].ID<t1){
                 cib=cible(tab,t1,tmax,n,2,1);
             } else {
