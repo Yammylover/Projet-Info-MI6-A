83174f93820904c7e332abfc199a076557dc5cb8
Kristal <gregoire.marie1@etu.cyu.fr>
2025-05-11 20:09:21 +0200

    Update capacites.c

--- capacites.c
+++ capacites.c
@@ -1,6 +1,6 @@
 #include "structure.h"
 
-void soin(Combattant* tab, int ID, int soin){
+void soin(Combattant* tab, int ID, int soin){    //soigne un combattant en ne dépassant pas les points de vie de base
     //check params
     if(tab==NULL || ID <0 || soin <0){
         printf("erreur soin\n");
@@ -14,25 +14,25 @@ void soin(Combattant* tab, int ID, int soin){
     }
 }
 
-void addeffect(Effet e, Combattant* tab, int ID){
+void addeffect(Effet e, Combattant* tab, int ID){    //augmente de 1 la taille du tableau effet et y ajoute un effet
     //locals
     Effet* temp;
     //process
-    temp=malloc((tab[ID].ne+1)*sizeof(Effet));
+    temp=malloc((tab[ID].ne+1)*sizeof(Effet));    //création tableau temporaire
     if(temp==NULL){
         printf("Erreur malloc addeffect\n");
         exit(65);
     }
-    for(int i=0; i<tab[ID].ne;i++){
+    for(int i=0; i<tab[ID].ne;i++){    //copie ancien tableau dans le temporaire
         temp[i]=tab[ID].effets[i];
         //("copie du %dème effet réussie!\n",i);
     }
-    temp[tab[ID].ne]=e;
+    temp[tab[ID].ne]=e;    //le dernier effet du temporaire devient l'effet ajouté
     tab[ID].ne++;
-    if(tab[ID].effets!=NULL){
+    if(tab[ID].effets!=NULL){    //free ancien tableau
         free(tab[ID].effets);
     }
-    tab[ID].effets=temp;
+    tab[ID].effets=temp;    //copie temp dans nouveau tableau
 }
 
 void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
@@ -44,20 +44,21 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
     //locals
     Effet e;
     int dg=0,cib=0,r=0;
+    //process
     switch (c){
         default:
             printf("ID de capacité non valide...Rien ne se passe!\n");
         break;
         
         case 5001:  //capacités de lambda
-            printf("%s|%d| utilise pose offensive!\n",tab[n].base.nom,tab[n].ID+1);
+            printf("%s|%d| utilise pose offensive!\n",tab[n].base.nom,tab[n].ID+1);    //ajoute un effet d'attaque
             e.type=2;
             e.puissance=tab[n].base.atk;
             e.duree=6;
             addeffect(e,tab,n);
             printf("%s|%d| gagne %d atk!\n",tab[n].base.nom,tab[n].ID+1,e.puissance);
         break;
-        case 5002:
+        case 5002:    //ajoute un effet de defense
             printf("%s|%d| utilise pose defensive!\n",tab[n].base.nom,tab[n].ID+1);
             e.type=3;
             e.puissance=tab[n].base.def;
@@ -65,13 +66,13 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             addeffect(e,tab,n);
             printf("%s|%d| gagne %d def!\n",tab[n].base.nom,tab[n].ID+1,e.puissance);
         break;
-        case 5003:
+        case 5003:    //soigne les pv
             printf("%s|%d| utilise repos!\n",tab[n].base.nom,tab[n].ID+1);
             soin(tab,n,50);
         break;
         
         case 5004:  //capacités de delta
-            if(tab[n].ID<t1){
+            if(tab[n].ID<t1){    //cible un ennemi, l'attaque et met à 0 son action
                 cib=cible(tab,t1,tmax,n,2,2);
             } else {
                 cib=cible(tab,t1,tmax,n,1,2);
@@ -92,7 +93,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
                 break;
             }
         break;
-        case 5005:
+        case 5005:    //gagne 2 effets
             printf("%s|%d| utilise aiguisage!\n",tab[n].base.nom,tab[n].ID+1);
             e.type=2;
             e.puissance=tab[n].base.atk*25/100;
@@ -105,7 +106,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             addeffect(e,tab,n);
             printf("%s|%d| gagne %d dex!\n",tab[n].base.nom,tab[n].ID+1,e.puissance);
         break;
-        case 5006:
+        case 5006:    //gagne 2 effets
             printf("%s|%d| utilise étirement!\n",tab[n].base.nom,tab[n].ID+1);
             e.type=4;
             e.puissance=tab[n].base.vit*50/100;
@@ -120,7 +121,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
         break;
         
         case 5007:  //capacités d'omega
-            if(tab[n].ID<t1){
+            if(tab[n].ID<t1){    //cible un ennemi et lui inflige un effet
                 cib=cible(tab,t1,tmax,n,2,2);
             } else {
                 cib=cible(tab,t1,tmax,n,1,2);
@@ -132,7 +133,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             addeffect(e,tab,cib);
             printf("%s|%d| perd %d atk!\n",tab[cib].base.nom,tab[cib].ID+1,-e.puissance);
         break;
-        case 5008:
+        case 5008:        //cible un allié et lui inflige un effet
             if(tab[n].ID<t1){
                 cib=cible(tab,t1,tmax,n,1,2);
             } else {
@@ -145,7 +146,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             addeffect(e,tab,cib);
             printf("%s|%d| gagne %d def!\n",tab[cib].base.nom,tab[cib].ID+1,e.puissance);
         break;
-        case 5009:
+        case 5009:    //parcourt toute l'équipe vivante et soigne les alliés
             printf("%s|%d| utilise soin de groupe!\n",tab[n].base.nom,tab[n].ID+1);
             if(tab[n].ID<t1){
                 for(int i=0;i<t1;i++){
@@ -164,7 +165,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
         break;
         
         case 5010:  //capacités d'epsilon
-            if(tab[n].ID<t1){
+            if(tab[n].ID<t1){    //cible un allié et lui inflige un effet
                 cib=cible(tab,t1,tmax,n,1,2);
             } else {
                 cib=cible(tab,t1,tmax,n,2,2);
@@ -176,7 +177,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             addeffect(e,tab,cib);
             printf("%s|%d| gagne %d vit!\n",tab[cib].base.nom,tab[cib].ID+1,e.puissance);
         break;
-        case 5011:
+        case 5011:    //cible un allié et le soigne
             if(tab[n].ID<t1){
                 cib=cible(tab,t1,tmax,n,1,2);
             } else {
@@ -185,7 +186,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             printf("%s|%d| utilise soin sur %s|%d| !\n",tab[n].base.nom,tab[n].ID+1,tab[cib].base.nom,tab[cib].ID+1);
             soin(tab,cib,40);
         break;
-        case 5012:
+        case 5012:    //cible un ennemi et le ralentit
             if(tab[n].ID<t1){
                 cib=cible(tab,t1,tmax,n,2,2);
             } else {
@@ -200,7 +201,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
         break;
         
         case 5013:  //capacités de alpha
-            if(tab[n].ID<t1){
+            if(tab[n].ID<t1){    //cible un ennemi, calcule les dégâts, les applique, puis se soigne
                 cib=cible(tab,t1,tmax,n,2,2);
             } else {
                 cib=cible(tab,t1,tmax,n,1,2);
@@ -224,7 +225,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
              }
         break;
         
-        case 5014:
+        case 5014:    //parcourt l'équipe pour donner un effet
             printf("%s|%d| utilise commandement!\n",tab[n].base.nom,tab[n].ID+1);
             e.type=2;
             e.duree=5;
@@ -246,7 +247,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
                 }
             }
         break;
-        case 5015:
+        case 5015:    //donne 3 effets
             printf("%s|%d| utilise inspiration!\n",tab[n].base.nom,tab[n].ID+1);
             e.type=4;
             e.puissance=tab[n].base.vit;
@@ -265,7 +266,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             printf("%s|%d| gagne %d dex!\n",tab[n].base.nom,tab[n].ID+1,e.puissance);
         break;
         //capacités beta
-        case 5016:
+        case 5016:    //cible un ennemi et attaque avec une valeur différente de 100
             if(tab[n].ID<t1){
                 cib=cible(tab,t1,tmax,n,2,2);
             } else {
@@ -286,7 +287,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
                 break;
             }
         break;
-        case 5017: 
+        case 5017: //applique un effet et se soigne
             printf("%s|%d| utilise barricade!\n",tab[n].base.nom,tab[n].ID+1);
             e.type=3;
             e.puissance=50;
@@ -295,7 +296,7 @@ void capacite(Combattant* tab, int n, int c, int t1, int t2, int tmax){
             printf("%s|%d| gagne %d def!",tab[n].base.nom,tab[n].ID+1,e.puissance);
             soin(tab,n,30);
         break;
-        case 5018:
+        case 5018:    //2 fois, chercher une cible aléatoire et attaquer avec un modificateur inférieur à 100
             printf("%s|%d| utilise trépignement!\n",tab[n].base.nom,tab[n].ID+1);
             if(tab[n].ID<t1){
                 cib=cible(tab,t1,tmax,n,2,1);
